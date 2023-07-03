4c567a07d6f81523eb3c633d0e076d8440163017
nikhilmalakar <nikhilmalakarofficial@gmail.com>
2023-07-03 18:28:33 +0530

    Initial Commit. Create basic structure

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import { CreatePost, Home } from "./pages";
+import logo from './assets/logo.svg'
+
+
+  function App(){
+    return (
+      
+    )
+  }
+
+export default App
